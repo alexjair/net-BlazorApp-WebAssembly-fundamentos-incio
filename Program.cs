b0f319bc27574caa51850d78b59d205572e97db8
Alexander Jair <alexjair@gmail.com>
2023-06-12 15:16:39 -0500

    Add json appseting

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+var apiUrl = builder.Configuration.GetValue<string>("ConfigApiUrl");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
